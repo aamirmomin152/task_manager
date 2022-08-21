012c61992627f01be3cd5468e0e5349e2b114b36
Momin <aamirmomin152@gmail.com>
2022-08-21 17:09:51 -0500

    Built out the JS variables for inputs by users

---
+++ app.js
@@ -0,0 +1,11 @@
+//Define UI variables
+
+
+const form = document.querySelector('#task-form');
+const tasklist = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#tasks');
+
+
+
