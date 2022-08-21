900c6acf83a7aadaf4cfb5a55bf230530ab386ac
Momin <aamirmomin152@gmail.com>
2022-08-21 17:24:28 -0500

    Add task functionality

--- app.js
+++ app.js
@@ -20,5 +20,26 @@ function addTask(e) {
     alert('Add a task')
   }
 
+  //Create li element
+  const li = document.createElement('li');
+  //Add class for styling
+  li.className = 'collection-item';
+  //Append the input from user to li child node
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  //Create new link element
+  const link = document.createElement('a');
+  //Add class for delete item
+  link.className = 'delete-item secondary-content';
+  //Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  //Append the link to li
+  li.appendChild(link);
+
+  //Append the li to the ul
+  tasklist.appendChild(li);
+  //Clear the input of the task
+  taskInput.value = '';
+
   e.preventDefault();
 }
