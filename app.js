f5a04e94c661788d910900383f02c63ff26d431c
Momin <aamirmomin152@gmail.com>
2022-08-21 17:16:59 -0500

    Added event listener for adding tasks

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-//Define UI variables
-
-
+// Define UI variables
 const form = document.querySelector('#task-form');
 const tasklist = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#tasks');
+const taskInput = document.querySelector('#task');
 
 
+// Load all event listeners
+loadEventListeners();
 
+function loadEventListeners() {
+  //Add task event
+  form.addEventListener('submit', addTask);
+}
+
+//Add task
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Add a task')
+  }
+
+  e.preventDefault();
+}
